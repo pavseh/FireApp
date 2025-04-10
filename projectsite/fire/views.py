4a7c9dcd98843dfdc5d509cee83df9690cfc7977
Theodore-Ross-18 <theodoreross18@gmail.com>
2025-04-10 10:59:28 +0800

    YES

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from fire.models import Locations, Incident
+from fire.models import Locations, Incident, FireStation
 
+def map_station(request):
+    fireStations = FireStation.objects.values('name', 'latitude', 'longitude')
+    
+    for fs in fireStations:
+        fs['latitude'] = float(fs['latitude'])
+        fs['longitude'] = float(fs['longitude'])
+    
+    fireStations_list = list(fireStations)
+
+    context = (
+        {
+            'fireStations': fireStations_list,
+        }
+    )
+    return render(request, 'map_station.html', context)
 
 class HomePageView(ListView):
     model = Locations
