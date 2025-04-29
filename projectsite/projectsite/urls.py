c0583a82f3145caf78090cbbdd95e9dc23de665d
Theodore-Ross-18 <theodoreross18@gmail.com>
2025-04-29 12:55:50 +0800

    Try

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from fire import views
-from fire.views import HomePageView, ChartView
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,4 +9,8 @@ urlpatterns = [
     path('stations', views.map_station, name='map-station'),
     path('maps/jqvmap.html', views.jqvmap_view, name='jqvmap'),
     path('chart/', ChartView.as_view(), name='chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountbyMonth, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
 ]
